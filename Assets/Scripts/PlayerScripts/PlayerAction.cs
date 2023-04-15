45b4fc638c5decb46abd6949592d1ff801d4c42b
Allerises <ibby.star.111@gmail.com>
2023-04-15 18:41:26 -0400

    Note penalization and a little postprocessing
    Added a variable "BeforeNotePadding" to MusicPlayer that creates a a zone where inputs are not penalized.
    
    Also added a postprocessing volume with some subtle effects.

--- Assets/Scripts/PlayerScripts/PlayerAction.cs
+++ Assets/Scripts/PlayerScripts/PlayerAction.cs
@@ -18,6 +18,7 @@ public abstract class PlayerAction : MonoBehaviour
     public double TimeStamp;
     protected double PerfectMarginOfError;
     protected double NiceMarginOfError;
+    protected double BeforeNotePadding;
     protected double AudioTime;
     public bool enableBlink;
     public bool enableArrows;
@@ -25,6 +26,7 @@ public abstract class PlayerAction : MonoBehaviour
     protected virtual void Start() {
         PerfectMarginOfError = MusicPlayer.Current.perfectMarginOfError;
         NiceMarginOfError = MusicPlayer.Current.niceMarginOfError;
+        BeforeNotePadding = MusicPlayer.Current.beforeNotePadding;
         AbleToBlink = true;
     }
 
@@ -63,7 +65,8 @@ public abstract class PlayerAction : MonoBehaviour
                 $"Hit inaccurate on {inputIndex} note with {Math.Abs(AudioTime - timeStamp)} delay - time: {timeStamp} audio time {AudioTime}");
             inputIndex++;
         }
-        else{
+        else if(Math.Abs(AudioTime - timeStamp) < BeforeNotePadding)
+        {
             //Oops
             Miss();
             print($"Missed {inputIndex} note - time: {timeStamp} audio time {AudioTime}");
@@ -102,7 +105,7 @@ public abstract class PlayerAction : MonoBehaviour
         if (curTimeStamp + NiceMarginOfError <= AudioTime)
         {
             Miss();
-            print($"Missed {inputIndex} note - time: {curTimeStamp} audio time {AudioTime}");
+            Debug.Log($"Missed {inputIndex} note - time: {curTimeStamp} audio time {AudioTime}");
             inputIndex++;
         }
         return inputIndex;
